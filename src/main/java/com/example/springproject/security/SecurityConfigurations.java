d61458d2aba36f14945e6b51ec48b74841f59dcf
Gustavo Irineu <101259023+gustavoib@users.noreply.github.com>
2023-08-04 15:58:48 -0300

    Update SecurityConfigurations.java

--- src/main/java/com/example/springproject/security/SecurityConfigurations.java
+++ src/main/java/com/example/springproject/security/SecurityConfigurations.java
@@ -16,7 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfigurations {
     @Autowired
     private SecurityFilter securityFilter;
